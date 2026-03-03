483a95d723d1c16e16e1d272c5efcff95818c793
mjmuhamm <53790225+mjmuhamm@users.noreply.github.com>
2026-03-03 10:21:13 -0500

    next steps

--- app/src/main/java/com/example/sharedflow/presentation/WeatherViewModel.kt
+++ app/src/main/java/com/example/sharedflow/presentation/WeatherViewModel.kt
@@ -1,4 +1,53 @@
 package com.example.sharedflow.presentation
 
-class WeatherViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sharedflow.domain.City
+import com.example.sharedflow.domain.WeatherRepository
+import com.example.sharedflow.domain.WeatherState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel @Inject constructor(
+    private val repository: WeatherRepository) : ViewModel() {
+
+        private val _uiState = MutableStateFlow(WeatherState())
+    val uiState = _uiState.asStateFlow()
+
+    private val _citySuggestions = MutableSharedFlow<List<City>>(replay = 1)
+    val citySuggestions = _citySuggestions.asSharedFlow()
+
+    private val _errorEvents = Channel<String>()
+    val errorEvents = _errorEvents.receiveAsFlow()
+
+    private var searchJob: Job? = null
+
+    fun onSearchQueryChanged(query: String) {
+        _uiState.update { it.copy(selectedCityName = query) }
+
+        searchJob?.cancel()
+
+        if(query.lowercase() == "error") {
+            viewModelScope.launch {
+                _errorEvents.send("Manual Error Triggered: 'error is a forbidden city!")
+
+            }
+            return
+        }
+
+        if (query.length < 3) {
+            viewModelScope.launch { _citySuggestions.emit(emptyList()) }
+            return
+        }
+    }
 }
